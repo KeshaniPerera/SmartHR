6032525c6a0a9dfa5aff3c7265e91c621e576d57
KeshaniPerera <160375688+KeshaniPerera@users.noreply.github.com>
2025-08-22 13:36:24 +0530

    Re arrange structure
    Re arrange structure

--- client/app/src/assets/pages/Home.jsx
+++
@@ -1,5 +0,0 @@
-function Home() {
-  return <div>This is Home Page</div>
-}
-
-export default Home;
--- server/SmartHR/HRApp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import login_api
-
-urlpatterns = [
-    path('api/login/', login_api),
-]
